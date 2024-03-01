6be6e12c4e565d0e2bc453705f481e4e518a04d9
anisham25 <anisha.musti@gmail.com>
2024-02-29 23:07:23 -0500

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 w-full p-4 bg-red-500">
+      <p className="text-black absolute bottom-0 w-full">© 2024 by Anisha Musti</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/app/serv-comp/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <main className="flex-grow pl-24 pt-24 pr-24">
+            <h1 className="text-violet-500 text-5xl font-semibold block pb-5">Server Components</h1>
+            <p className="text-white text-xl block">
+                React Server Components are UI that are rendered on the server. There are three types of components:<br />
+                <b>Static Rendering:</b> Routes are rendered at build time and cached and pushed to a CDN. This is useful when a route has data that is not personalized to the user.<br />
+                <b>Dynamic Rendering:</b> Routes are rendered for each user at request time. This is useful when the components contain data that is personalized to the user.<br />
+                <b>Streaming:</b> Streaming enables you to progressively render UI from the server. Work is split into chunks and streamed to the client as it becomes ready.<br />
+            </p>
+        </main>
+    )
+}
